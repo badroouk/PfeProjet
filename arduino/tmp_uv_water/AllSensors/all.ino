ce156b4950ffafcac047b5022df28ea62898dae5
Reda Ghadouani <93874762+RedaG1@users.noreply.github.com>
2022-05-15 10:52:38 +0100

    Update all.ino

--- arduino/tmp_uv_water/AllSensors/all.ino
+++ arduino/tmp_uv_water/AllSensors/all.ino
@@ -21,29 +21,29 @@ void loop(){
   float h = dht.readHumidity();                                              // Reading temperature or humidity takes about 250 milliseconds!
   delay(250);
   float t = dht.readTemperature();
-  Serial.print("Humidity: ");  
-  Serial.print(h);
-  Serial.print("%  Temperature: ");  
-  Serial.print(t);  
-  Serial.println("°C ");
-  delay(1000);
+//   Serial.print("Humidity: ");  
+//   Serial.print(h);
+//   Serial.print("%  Temperature: ");  
+//   Serial.print(t);  
+//   Serial.println("°C ");
+//   delay(1000);
 
   //UV Sensor
   int ad_value=analogRead(uv_ain);
-  Serial.print("UV value: ");
-  Serial.println(ad_value);
-  delay(1000);
+//   Serial.print("UV value: ");
+//   Serial.println(ad_value);
+//   delay(1000);
 
   //Water Sensor
   int sensor=analogRead(A0); // Incoming analog signal read and appointed sensor
-   Serial.print("Precipitation :");
-  Serial.println(sensor);
-  delay(1000);
+//    Serial.print("Precipitation :");
+//   Serial.println(sensor);
+//   delay(1000);
 
   //Photoresistor
   photocellReading = analogRead(photocellPin);
-  Serial.print("Luminosity : ");
-  Serial.print(photocellReading);     // the raw analog reading
-  Serial.println(" LUX");
+//   Serial.print("Luminosity : ");
+//   Serial.print(photocellReading);     // the raw analog reading
+//   Serial.println(" LUX");
   delay(1000);
 }
