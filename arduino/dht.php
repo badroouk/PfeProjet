fdd0a8eee3d970933ba3a0e09a5abc927d1885ca
Reda Ghadouani <93874762+RedaG1@users.noreply.github.com>
2022-05-17 10:53:00 +0100

    Add files via upload

---
+++ arduino/dht.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<?php
+
+$dbname = 'iot';
+$dbuser = 'root';  
+$dbpass = ''; 
+$dbhost = 'localhost'; 
+
+$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$connect){
+	echo "Error: " . mysqli_connect_error();
+	exit();
+}
+
+echo "Connection Success!<br><br>";
+
+$temperature = $_GET["temperature"];
+$humidity = $_GET["humidity"]; 
+$ultraviolet = $_GET["ultraviolet"];
+$precipitation = $_GET["precipitation"];
+$luminosity = $_GET["luminosity"];
+$carbonmonoxide = $_GET["carbonmonoxide"];
+
+
+$query = "INSERT INTO arduino (temperature, humidity, ultraviolet, precipitation, luminosity, carbonmonoxide) VALUES ('$temperature', '$humidity', '$ultraviolet', '$precipitation', '$luminosity', '$carbonmonoxide')";
+$result = mysqli_query($connect,$query);
+
+echo "Insertion Success!<br>";
+
+?>
+</body>
+</html>
